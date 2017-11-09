6249e8df0cdd7d9f17488cde2e2143b404c1b422
rramos20 <rramos20@mail.csuchico.edu>
2017-11-08 17:39:26 -0800

    added more functions

--- main.cpp
+++ main.cpp
@@ -5,11 +5,16 @@
 #include <vector>
 using namespace std;
 //Prototypes
-string fileName();
-bool validBMP(vector<string> &);
+vector <string> fileName(); //Asks for users input
+
+bool validBMP(vector<vector<Pixel> >);//Checks to see if file is valid bitmap
+
+void rgbValues();//Gets RGB values for each pixel
+
+bool BMPsize();//Checks size of picture to make sure it is the right dimensions
 
 
-//Global Variable
+//Global Variables
 const int MAX_FILES = 10;
 Bitmap image;
 vector <vector <Pixel > > bmp;
@@ -19,32 +24,48 @@ Pixel rgb;
 //Main Program
 int main()
 {
-vector <string> input;
-for(int i=0; i<MAX_FILES;i++)
+fileName();
+
+/*for(int i=0; i<MAX_FILES;i++)
     {   
     input.push_back( fileName() );
     }
-    
+  */  
 return 0;
 
 }
 
-//Asks user for file names 
-/*string fileName()
+//Asks user for file names until they enter 10 names or type "Done"
+vector <string> fileName()
 {
-vector <string> files;
+vector<string> files;
 cout<<"Please enter filename(s) or type 'Done' when finished."<<endl;
-cin >> files;
+cin>>files;
+validBMP(files);
+while(validBMP = false;)
+    {
+        cout<<"Please make sure the images are all the same size and valid Bitmap files."
+        cin>>files;
+        validBMP(files);
+    }
+return fileName;
+}
 
-return files;
-}*/
-
-
+bool validBMP(vector <string> images)
+{
+    for(int i=0;i<images.size;i++)
+    {
+        if(images[i] == images[i].IsImage)
+        {
+    return true;
+        }
+else
+return false;
+    }
+}
 //Repeatedly ask user for their filename until they enter "Done" or entered 10 file names
 
 //print an error for any filename with a problem
-
-//take average RGB values from all pictures and average them 
 
 //print updates for the user 
 
